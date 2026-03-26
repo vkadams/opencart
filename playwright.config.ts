f5b9fa27f7d49f5e094f08728106afe25d212604
Varun Kadam <varun.61015706@ltimindtree.com>
2026-03-26 15:31:39 +0530

    add necessary test configuration

---
+++ playwright.config.ts
@@ -0,0 +1,45 @@
+import {defineConfig, devices} from '@playwright/test';
+
+export default defineConfig({
+  timeout: 30 * 1000, // 30000ms (30 secs)
+  testDir: './tests',
+  fullyParallel: false,
+  //retries: process.env.CI ? 2 : 0,
+  retries: 1,
+  //workers: process.env.CI ? 1 : undefined,
+  workers:1,
+
+  reporter: [
+    ['html'],
+    ['allure-playwright'],
+    ['dot'],
+    ['list']
+  ],
+
+  use: {
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+
+    //headless: false,
+    viewport: {width: 1280, height: 1024}, // set default viewport size for consistency
+    ignoreHTTPSErrors: true,// Ignore SSL errors if necessart
+    permissions: ['geolocation'], // set necessary permissions for geolocation-based tests
+  },
+  //grep: /@master/,
+  
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome']},
+    },
+    /*{
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox']},
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari']},
+    }*/
+  ],
+})
---
+++ test.config.ts
@@ -0,0 +1,11 @@
+export class TestConfig{
+//appUrl="http://tutorialsninja.com/demo/"
+appUrl="http://naveenautomationlabs.com/opencart/"
+//valid login credentials- create your own login account
+email="pavanol@abc.com"
+password="test@123"
+//product details
+productName="MacBook"
+productQuantity="2"
+totalPrice="$1,204.00"
+}
